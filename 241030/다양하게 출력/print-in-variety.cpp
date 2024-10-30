96afc63f388642e86bfd1d6a4dd7cf74be9b85b4
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-10-30 19:02:50 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241030/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "Total days in Year\n";
+    cout << 365 << endl;
+    cout << "Circumference rate\n";
+    cout << fixed << setprecision(10) << 3.1415926535;
+    return 0;
+}
