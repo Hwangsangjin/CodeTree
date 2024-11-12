7b226b26d35ffbae93cae13bfba1020d7f429915
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-12 18:44:49 +0900

    Title: 전화번호 바꾸기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241112/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Front, Middle, Back;
+    cin >> Front;
+    cin.get();
+    cin >> Middle;
+    cin.get();
+    cin >> Back;
+    cout << "010-" << Back << "-" << Middle;
+    return 0;
+}
