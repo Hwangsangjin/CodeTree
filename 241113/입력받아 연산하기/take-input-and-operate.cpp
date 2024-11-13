264bef4961c3a63e42a7058074b1a282f991eea2
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-13 21:01:48 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241113/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    A += 87;
+    B %= 10;
+
+    cout << A << endl;
+    cout << B;
+    return 0;
+}
