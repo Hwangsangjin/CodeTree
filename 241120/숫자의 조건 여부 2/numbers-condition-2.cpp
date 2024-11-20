f6faab44d182e2d8816b6bd5bf33679c44997056
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-20 20:19:34 +0900

    Title: 숫자의 조건 여부 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241120/숫자의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+
+    if (A == 5)
+    {
+        cout << 'A';
+    }
+
+    if (A % 2 == 0)
+    {
+        cout << 'B';
+    }
+
+    return 0;
+}
