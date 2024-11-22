c98eb60f3a60373af23a0e7d545e5accacf27ef9
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-22 18:15:37 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241122/비교 연산/comparison-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    cout << (A >= B) << endl;
+    cout << (A > B) << endl;
+    cout << (B >= A) << endl;
+    cout << (B > A) << endl;
+    cout << (A == B) << endl;
+    cout << (A != B);
+    return 0;
+}
