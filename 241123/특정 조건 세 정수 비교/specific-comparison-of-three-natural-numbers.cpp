b2bb1bffc6e83c50b1c1b372b5d7a1d17a141c0c
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-23 10:40:04 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241123/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 241123/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int A, B, C;
     cin >> A >> B >> C;
-    cout << (A < B && A < C) << " ";
+    cout << (A <= B && A <= C) << " ";
     cout << (A == B && A == C); 
     return 0;
 }
