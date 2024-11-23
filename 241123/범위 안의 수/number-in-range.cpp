670ee2c52e99ba605d2a4f77b1e1102054a99407
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-23 10:32:15 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241123/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+
+    if (10 <= A && A <= 20)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+
+    return 0;
+}
