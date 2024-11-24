0b7bb516556978e36247d34613133b7fa0ae976a
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-24 16:05:41 +0900

    Title: 범위 밖의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241124/범위 밖의 수/number-out-of-range.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+
+    if (A < 10 || 20 < A)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+
+    return 0;
+}
