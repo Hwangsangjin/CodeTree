2679b1f1a15f220a1128c4ab1bf66193194bb3dc
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-26 20:32:36 +0900

    Title: 윤년인가, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241126/윤년인가/is-leap-year.cpp
+++ 241126/윤년인가/is-leap-year.cpp
@@ -6,7 +6,7 @@ int main()
     int Year;
     cin >> Year;
 
-    if ((Year % 4 == 0) || (Year % 100 != 0 && Year % 400 == 0))
+    if ((Year % 4 == 0) || (Year % 100 != 0 || Year % 400 == 0))
     {
         cout << "true";
     }
