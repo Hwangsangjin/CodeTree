29fcad5c65dfc47cb8caed8e4989889edd3c1860
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-26 21:19:50 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241126/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ASymptom, BSymptom, CSymptom;
+    int ATemperature, BTemperature, CTemperature;
+
+    cin >> ASymptom >> ATemperature;
+    cin >> BSymptom >> BTemperature;
+    cin >> CSymptom >> CTemperature;
+
+    int Count = 0;
+
+    if (ASymptom == 'Y' && ATemperature >= 37)
+    {
+        Count++;
+    }
+
+    if (BSymptom == 'Y' && BTemperature >= 37)
+    {
+        Count++;
+    }
+
+    if (CSymptom == 'Y' && CTemperature >= 37)
+    {
+        Count++;
+    }
+    
+    cout (Count >= 2 ? 'E' : 'N');
+    return 0;
+}
