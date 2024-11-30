2c97629b6beb077ae958d0f07dcdfaa526338650
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-30 16:45:27 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Index = 10;
+
+    while (Index <= 26)
+    {
+        cout << Index << " ";
+        Index++;
+    }
+
+    return 0;
+}
