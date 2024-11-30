31bb9829f8f9229dc38e557832c27be7cdf9eeeb
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-11-30 16:47:17 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int Index = 1;
+
+    while (Index <= N)
+    {
+        cout << Index << " ";
+        Index++;
+    }
+    
+    return 0;
+}
