093631958a9797cec23936fbeea515f07653167c
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-02 16:52:58 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241202/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Index = 26;
+
+    while (Index >= 10)
+    {
+        cout << Index << " ";
+        Index--;
+    }
+
+    return 0;
+}
