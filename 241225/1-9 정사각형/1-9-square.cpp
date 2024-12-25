cc4187b873c8c507daf3100fb48d635701aaa5a8
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-25 22:54:09 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241225/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, Count = 1;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << Count++;
+
+            if (Count == 10)
+                Count = 1;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
