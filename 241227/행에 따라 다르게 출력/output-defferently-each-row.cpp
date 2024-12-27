a83cf4b92bdc3d70451f6a6b1da81ad67dee04de
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-27 12:09:53 +0900

    Title: 행에 따라 다르게 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241227/행에 따라 다르게 출력/output-defferently-each-row.cpp
+++ 241227/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -3,29 +3,14 @@ using namespace std;
 
 int main()
 {
-    int N, Count = 1;
+    int N, Count = 0;
     cin >> N;
 
     for (int i = 0; i < N; ++i)
     {
         for (int j = 0; j < N; ++j)
         {
-            cout << Count << ' ';
-
-            if (i % 2 == 0)
-            {
-                Count++;
-
-                if (j == N - 1)
-                    Count++;
-            }
-            else
-            {
-                Count += 2;
-
-                if (j == N - 1)
-                    Count--;
-            }
+            cout << (i % 2 == 0 ? ++Count : Count += 2) << ' ';
         }
 
         cout << '\n';
