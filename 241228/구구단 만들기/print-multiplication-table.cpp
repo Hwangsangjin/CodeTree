8e39107c5b1346c1f7924687e66e581ae2cfb852
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-28 20:38:44 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241228/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    for (int i = 1; i <= 9; ++i)
+    {
+        for (int j = B; j >= A; j -= 2)
+        {
+            cout << j << " * " << i << " = " << j * i;
+
+            if (j != A)
+                cout << " / ";
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
