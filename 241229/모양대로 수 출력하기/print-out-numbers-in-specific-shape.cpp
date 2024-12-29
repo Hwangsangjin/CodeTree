b1f1bfa8f6cba8507f44087af8f6c2bd34897dd8
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-29 18:13:54 +0900

    Title: 모양대로 수 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cout << "  ";
+        }
+
+        for (int j = 0; j < N - i; ++j)
+        {
+            cout << N - i - j << ' ';
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
