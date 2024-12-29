837348c4d090f771a37aefe0c76ac6e04282aa0d
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-12-29 17:49:03 +0900

    Title: 숫자 피라미드 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, Count = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < i + 1; ++j)
+        {
+            cout << ++Count << ' ';
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
