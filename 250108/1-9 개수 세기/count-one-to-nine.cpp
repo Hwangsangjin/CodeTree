92047be98a0519c487b8fcdd8014b907c4a5b697
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-01-08 23:50:46 +0900

    Title: 1-9 개수 세기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250108/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Array[100];
+    int Count[10] = {};
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+        cin >> Array[i];
+    
+    for (int i = 0; i < 10; ++i)
+    {
+        if (Array[i] >= 1 && Array[i] <= 9)
+            Count[Array[i]]++;
+    }
+        
+    for (int i = 1; i < 10; ++i)
+        cout << Count[i] << "\n";
+
+    return 0;
+}
