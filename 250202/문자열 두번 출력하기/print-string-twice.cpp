e0209a8b85b21f7f84f363803087d67faca3d6c2
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-02-02 22:48:31 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    for (int i = 0; i < 2; ++i)
+        cout << S << "\n";
+
+    return 0;
+}
