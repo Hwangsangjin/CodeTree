7ebd7acfafeb124705290bde030b8d984f5938cb
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-02-08 23:43:51 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250208/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string Arr[10];
+
+    for (int i = 0; i < 10; ++i)
+        cin >> Arr[i];
+
+    for (int i = 0; i < 10; i += 2)
+        cout << Arr[i] << "\n";
+
+    return 0;
+}
