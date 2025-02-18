40ab4567a5baca50406de32f2112d2a716ecea1d
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-02-18 22:24:00 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250218/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char A, B;
+    cin >> A >> B;
+    cout << (int)A + (int)B << " " << abs((int)A - (int)B) << endl;
+    return 0;
+}
