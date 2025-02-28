5dfa86683174a66e453d92576c1f66bceeac879b
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-02-28 22:01:57 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double A, B, C;
+    cin >> A >> B >> C;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << A << "\n" << B << "\n" << C;
+    return 0;
+}
