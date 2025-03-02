cf9b4e8638c77513bde94bafc1d75c4978ed8f36
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-03-02 17:24:31 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250302/물의 상태/state-of-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    if (N < 0)
+        cout << "ice";
+    else if (N >= 100)
+        cout << "vapor";
+    else
+        cout << "water";
+
+    return 0;
+}
