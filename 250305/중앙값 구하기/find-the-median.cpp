53a00253696a282c0f14afe8188db0fce7aa5831
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-03-05 23:13:12 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250305/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if ((A >= B && A <= C) || (A >= C && A <= B))
+        cout << A;
+    else if ((B >= A && B <= C) || (B >= C && B <= A))
+        cout << B;
+    else
+        cout << C;
+
+    return 0;
+}
