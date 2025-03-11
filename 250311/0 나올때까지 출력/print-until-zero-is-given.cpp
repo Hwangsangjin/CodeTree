3d105851a982bcf25f728a80bcbb1e28e8bcc7b4
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-03-11 22:50:55 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250311/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    while (true)
+    {
+        int N;
+        cin >> N;
+
+        if (N == 0)
+            break;
+        
+        cout << N << "\n";
+    }
+
+    return 0;
+}
