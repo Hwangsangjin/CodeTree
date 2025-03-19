b3738b75130690f6cd55667d8e3821a1b41dc4ba
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-03-19 22:31:43 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250319/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Print(int N)
+{
+    for (int i = 0; i < N; ++i)
+        cout << "12345^&*()_\n";
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    Print(N);
+    return 0;
+}
