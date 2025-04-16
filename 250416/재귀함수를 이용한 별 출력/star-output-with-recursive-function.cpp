8fe05a92c11bd28fa2d39686da36255900a85104
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-04-16 23:19:39 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void Answer(int N)
+{
+    if (N == 0)
+        return;
+    
+    Answer(N - 1);
+
+    for (int i = 0; i < N; ++i)
+        cout << "*";
+    cout << "\n";
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    Answer(N);
+    return 0;
+}
