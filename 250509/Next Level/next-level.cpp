69d840cfc4075faf42ed823f4e8f11d7b86b5c28
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-05-09 16:38:15 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250509/Next Level/next-level.cpp
+++ 250509/Next Level/next-level.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <tuple>
+#include <utility>
 #include <string>
 using namespace std;
 
 int main()
 {
-    tuple<string, int> User1 = make_tuple("codetree", 10);
-    tuple<string, int> User2;
+    pair<string, int> User1 = make_pair("codetree", 10);
+    pair<string, int> User2;
 
     string ID;
     int Level;
     cin >> ID >> Level;
 
-    User2 = make_tuple(ID, Level);
+    User2 = make_pair(ID, Level);
 
-    cout << "user " << get<0>(User1) << " lv " << get<1>(User1) << endl;
-    cout << "user " << get<0>(User2) << " lv " << get<1>(User2) << endl;
+    cout << "user " << User1.first << " lv " << User1.second << endl;
+    cout << "user " << User2.first << " lv " << User2.second << endl;
 
     return 0;
 }
